025f0b3e0ba0db8d8e87d1ff751a4375a0114f0b
sheilabett <sheilahbett@gmail.com>
2023-09-23 09:56:14 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+ // if (customerName) {}
+    customerName = customerName.toUpperCase();
+  
+}
+
+// Calling the function to uppercase customerName
+
+
+//console.log(customerName); 
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+
+const leastFavoriteCustomer = 'name';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer='name';}
